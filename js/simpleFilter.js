b90cdbc4beff5de647df28def340e4b727055d40
Federico Nurra <nurrafederico@gmail.com>
2020-02-20 14:50:46 +0100

    Create simpleFilter.js

---
+++ js/simpleFilter.js
@@ -0,0 +1,37 @@
+// Ce script nécessite de jquery
+// Lorsque la page charge...
+$(document).ready(function(){
+// Définir la touche "entrée" dans l'input (modifier l'idInput avec le bon id)
+	$('#idInput').keyup(function(e){
+	var code = e.key;
+// Evite que la touche « entrée » fasse d'autres actions
+	if(code==="Enter") e.preventDefault();
+// À la pression de la touche "entrée"
+	if(code==="Enter") {
+// cette variable définit le filtre sur la base de ce qu'on écrit dans le formulaire d'input (modifier l'idInput avec le bon id)
+// toLowerCase() : tranforme en minuscule et rend la recherche insensible au Majuscule/minuscule
+// normalize("NFD") et replace([...]): transforment les lettres accentuées et rendent insensible aux accents
+		var value = $('#idInput').val().toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+// cette variable définit les éléments à montrer (changer le tag selon votre nécessité - ex. img, div, etc.)
+		var showedElements = $('tag').filter(function() {
+// cette fonction vérifie si le résultat colle bien avec la recherche (changer l'attribute - ex. class, title, etc.)
+// toLowerCase() : tranforme en minuscule et rend la recherche insensible au Majuscule/minuscule
+// normalize("NFD") et replace([...]): transforment les lettres accentuées et rendent insensible aux accents
+		return $(this).attr('attribute').toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "").indexOf(value) > -1;
+		});
+// cette variable définit les éléments à cacher (changer le tag selon votre nécessité - ex. img, div, etc.)
+		var hiddenElements = $('tag').filter(function() {
+// vérifier si le résultat ne colle pas avec la recherche (changer l'attribute - ex. class, title, etc.)
+// toLowerCase() : tranforme en minuscule et rend la recherche insensible au Majuscule/minuscule
+// normalize("NFD") et replace([...]): transforment les lettres accentuées et rendent insensible aux accents
+		return $(this).attr('attribute').toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "").indexOf(value) == -1;
+		});
+// cette fonction montre les résultats qui collent
+		showedElements.show();
+// cette fonction cache les résultats qui collent
+		hiddenElements.hide();
+// cette fonction vide la barre de l'input suite à la recherche (modifier l'idInput avec le bon id)
+		$('#idInput').val('');
+	}
+	});
+});
